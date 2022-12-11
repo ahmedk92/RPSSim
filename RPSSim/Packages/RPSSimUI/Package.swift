787c1a39648213f046e1ecef01c2a962c8d8a537
Ahmed Khalaf <ahmedkhalaf.92@gmail.com>
2022-12-11 23:32:30 +0200

    Fix package dependency

--- RPSSim/Packages/RPSSimUI/Package.swift
+++ RPSSim/Packages/RPSSimUI/Package.swift
@@ -24,7 +24,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "RPSSimUI",
-            dependencies: []),
+            dependencies: ["RPSSimCore"]),
         .testTarget(
             name: "RPSSimUITests",
             dependencies: ["RPSSimUI"]),
